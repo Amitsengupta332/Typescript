b8e2cf69ebafe199c50cfb92fc62142ec91f54ca
amitsengupta <amitsengupta332@gmail.com>
2025-11-08 23:22:05 +0600

    Explore Non Primitive Types

---
+++ Basic_Typescript/src/non-primitive.ts
@@ -0,0 +1,15 @@
+// Array , Object
+
+// Ts - tuple
+
+let bazarList: string[] = ['eggs', 'milk', 'sugar'];
+
+// bazarList.push(12);
+// bazarList.push('chocolate');
+// bazarList.push(123);
+
+let coOrdinates: [number, number] = [20,30];
+
+let couple: [string, string] = ['Amol', 'Pooja'];
+
+let amitNameandRoll: [string, number] = ['Amit', 123];
