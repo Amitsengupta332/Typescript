da99688b128b5e9a79e0dd003160d909829ddde7
amitsengupta <amitsengupta332@gmail.com>
2025-11-08 22:57:57 +0600

    Add initial TypeScript files and configuration

---
+++ Basic_Typescript/src/primitive.ts
@@ -0,0 +1,12 @@
+// Js - TS
+// string , number, boolean, undefined, null
+// primitive data type
+
+//  TS: never, any, unknown, void
+
+let userName: string = 'amit123';
+
+// userName = 123;
+
+let userId = 123;
+// userId = '123';
