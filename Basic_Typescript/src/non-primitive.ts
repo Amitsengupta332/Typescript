1eb4ccda660659a6b219a50ea83aa98107a82672
amitsengupta <amitsengupta332@gmail.com>
2025-11-08 23:44:20 +0600

    Object , Literal & Optional Type

--- Basic_Typescript/src/non-primitive.ts
+++ Basic_Typescript/src/non-primitive.ts
@@ -2,14 +2,34 @@
 
 // Ts - tuple
 
-let bazarList: string[] = ['eggs', 'milk', 'sugar'];
+let bazarList: string[] = ["eggs", "milk", "sugar"];
 
 // bazarList.push(12);
 // bazarList.push('chocolate');
 // bazarList.push(123);
 
-let coOrdinates: [number, number] = [20,30];
+let coOrdinates: [number, number] = [20, 30];
 
-let couple: [string, string] = ['Amol', 'Pooja'];
+let couple: [string, string] = ["Amol", "Pooja"];
 
-let amitNameandRoll: [string, number] = ['Amit', 123];
+let amitNameandRoll: [string, number] = ["Amit", 123];
+
+// reference type : object type
+
+const user: {
+  firstName: string;
+  middleName?: string;
+  lastName: string;
+  isMarried: boolean;
+} = {
+  firstName: "Amit",
+  //   middleName: "",
+  //   middleName: "SenGupta",
+  lastName: "SenGupta",
+  isMarried: false,
+  //   lastName: "Gupta",
+  //   age: 23,
+  //   roll: 123,
+};
+
+ 
