36aa624f2459d5d1b5a2d76f437140145d8412ba
amitsengupta <amitsengupta332@gmail.com>
2025-11-09 21:11:35 +0600

    Destructuring in typescript

---
+++ Basic_Typescript/src/destructuring.ts
@@ -0,0 +1,26 @@
+// object destructuring
+// array destructuring
+
+const user = {
+  id: 123,
+  name: {
+    firstName: "Mezbaul",
+    middleName: "Abedin",
+    lastName: "Forhan",
+  },
+  gender: "male",
+  favouriteColor: "black",
+};
+
+//const myFavouriteColor = user.favouriteColor
+//const myMiddleName = user.name.middleName
+
+const { favouriteColor, name: myMiddleName } = user;
+
+console.log(myMiddleName);
+
+const friends = ["karim", "Rahim", "Mahim"];
+
+const [, , myBestFriend] = friends;
+
+console.log(myBestFriend);
